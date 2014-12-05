4acf6c1e5dcb376e01a5e469404b52b91e5917e4
Melon <melonq@outlook.com>
2014-12-05 13:31:08 +0800

    change payment.erb to display inventory

--- app.rb
+++ app.rb
@@ -124,19 +124,7 @@ class POSApplication < Sinatra::Base
         @shopping_cart = ShoppingCart.new()
         @shopping_cart.init_with_Data cart_data
         @shopping_cart.update_price
-        puts "================"
-        puts "总价格：" + format("%0.2f", @shopping_cart.sum_price) + " 总折扣：" + format("%0.2f", @shopping_cart.sum_discount)
-        puts "----------------"
-        @shopping_cart.shopping_list.each do |item|
-            puts item.name + "购买" + item.amount.to_s + item.unit + "，实际价格" + format("%0.2f", item.kindred_price)
-        end
-        puts "----------------"
-        @shopping_cart.discount_list.each do |discount_item|
-            puts discount_item.name + "打折" + discount_item.discount_amount.to_s + discount_item.unit
-        end
-        puts "================"
-        #渲染
-        #返回对应的erb
+        
         content_type :html
         erb :'/pages/payment'
     end
